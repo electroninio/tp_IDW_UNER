9c857e731e63d4ac8c4a182ccb36c94315c24cc8
electroninio <javier.gerol@gmail.com>
2024-04-27 16:36:50 -0300

    actualizado

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configura Express para servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta de inicio
+app.get('/',(req,res)=>{
+  res.sendFile(join(__dirname,'public/index.html'));
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
